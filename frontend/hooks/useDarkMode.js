fd3206929bed7291be465e42d15e92d436387646
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-11 16:52:33 -0700

    Completed construction of first hook

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 
-const useDarkMode = () => {
-    
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useState(initialValue);
+    return [darkMode, setDarkMode];
 }
+
+export default useDarkMode;
